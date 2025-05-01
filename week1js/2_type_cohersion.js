b9b80a9d7ac32a035a7e920eeddf5a157280ae82
Tupesh <tupeshghimire@gmail.com>
2025-05-01 22:41:59 +0545

    type cohersion

---
+++ week1js/2_type_cohersion.js
@@ -0,0 +1,12 @@
+var a =35;
+var b= "12";
+console.log(a+b); // Converts a into string data
+console.log(a-b) // Converts b into numeric data
+console.log(b-a); // Converts b into numeric data
+console.log(true-a); // Converts True into 1 and gives 1 -35
+console.log(true && 0); // Takes 0 as false
+console.log(false || 1); // Takes 1 as true
+console.log(true + true); // Add 1 and 1 as 1 is taken as true
+
+// == compares the values only not the data type
+// === strictly compare the values and data type
